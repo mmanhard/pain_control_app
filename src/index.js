3190f99e6894f91854e43c40f5e17018ded7ab02
Michael Manhard <mmanhard93@gmail.com>
2020-05-16 13:15:33 -0500

    -Set up webpack.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 console.log("App is running");
-var index = <p>This is a test!!!</p>
+const index = <p>This is a test!!! x 8</p>;
 
 const appRoot = document.getElementById('app');
 ReactDOM.render(index, appRoot);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  watch: true,
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, "public")
+  }
+};
