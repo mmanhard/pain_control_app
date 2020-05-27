6fa9b4ac676a2f7b92670a6a66a2be9ce8ef4331
Michael Manhard <mmanhard93@gmail.com>
2020-05-26 22:39:40 -0500

    -Add persistence to the redux store.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from "react-redux";
 
 import Navigation from './navigation';
-import { Provider } from "react-redux";
 import configureStore from "./store";
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 const appRoot = document.getElementById('app');
 const app = (
   <Provider store={store}>
-    <Navigation />
+    <PersistGate loading={null} persistor={persistor}>
+      <Navigation />
+    </PersistGate>
   </Provider>
 );
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,18 @@
 import { createStore, applyMiddleware } from "redux";
 import { appReducer } from "../reducers";
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, appReducer)
 
 export default () => {
-  return createStore(appReducer, applyMiddleware(thunk));
-};
+  const store = createStore(persistedReducer, applyMiddleware(thunk));
+  const persistor = persistStore(store)
+  return { store, persistor }
+}
