79ee47bb944a387acc45d2750db25e7760a5a604
Michael Manhard <mmanhard93@gmail.com>
2020-06-01 13:09:26 -0500

    -Add all pages to the router.

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -18,11 +18,29 @@ class Navigation extends React.Component {
             <Route path="/" exact={true}>
               <Pages.Login />
             </Route>
-            <PrivateRoute path="/onboarding" isLogin={this.props.isLogin}>
+            <PrivateRoute path="/onboarding" isLogin={this.props.isLogin} exact={true}>
               <Pages.Onboarding />
             </PrivateRoute>
-            <PrivateRoute path="/dashboard" isLogin={this.props.isLogin}>
+            <PrivateRoute path="/dashboard" isLogin={this.props.isLogin} exact={true}>
               <Pages.Dashboard />
+            </PrivateRoute>
+            <PrivateRoute path="/entries" isLogin={this.props.isLogin} exact={true}>
+              <Pages.Entries />
+            </PrivateRoute>
+            <PrivateRoute path="/entries/:entryID" isLogin={this.props.isLogin}>
+              <Pages.EntryDetail />
+            </PrivateRoute>
+            <PrivateRoute path="/add_entry" isLogin={this.props.isLogin} exact={true}>
+              <Pages.AddEntry />
+            </PrivateRoute>
+            <PrivateRoute path="/pain_points" isLogin={this.props.isLogin} exact={true}>
+              <Pages.PainPoints />
+            </PrivateRoute>
+            <PrivateRoute path="/pain_points/:pointID" isLogin={this.props.isLogin}>
+              <Pages.PainPointDetail />
+            </PrivateRoute>
+            <PrivateRoute path="/settings" isLogin={this.props.isLogin} exact={true}>
+              <Pages.Settings />
             </PrivateRoute>
           </Switch>
         </div>
