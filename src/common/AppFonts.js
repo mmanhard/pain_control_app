0172bce455ed933eb9f24dee474ff4e23c72c8aa
Michael Manhard <mmanhard93@gmail.com>
2020-06-08 12:12:42 -0500

    -Add app fonts file.

---
+++ src/common/AppFonts.js
@@ -0,0 +1,22 @@
+const Raleway = {
+  light: {
+    fontFamily: 'Raleway',
+    fontWeight: '200'
+  },
+  regular: {
+    fontFamily: 'Raleway',
+    fontWeight: '400'
+  },
+  bold: {
+    fontFamily: 'Raleway',
+    fontWeight: '700'
+  },
+  xBold: {
+    fontFamily: 'Raleway',
+    fontWeight: '900'
+  },
+}
+
+export default {
+  Raleway
+}
