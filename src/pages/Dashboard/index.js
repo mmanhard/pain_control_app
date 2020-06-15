4599531141d1ea33011583b2eee05727db041a13
Michael Manhard <mmanhard93@gmail.com>
2020-06-14 19:53:38 -0500

    -Add basic styling to dashboard page.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -5,6 +5,7 @@ import { withRouter } from 'react-router';
 import actions from 'Actions';
 import Navbar from 'Components/Navbar';
 import styles from './style';
+import AppStyles from 'Common/AppStyles';
 
 const statTypes = {
   avg: 'avg',
@@ -43,32 +44,42 @@ class Dashboard extends React.Component {
       <div style={styles.container}>
         <Navbar userInfo={userInfo} logout={logout}/>
         <div style={styles.contentContainer}>
-          <h3>User ID: {this.props.userInfo?.id}</h3>
-          <h3>Name: {`${userInfo?.first_name} ${userInfo?.last_name}`}</h3>
-          <h3>{userInfo?.email && `Email: ${userInfo?.email}`}</h3>
-          <h3>{userInfo?.phone && `Phone Number: ${userInfo?.phone}`}</h3>
-          <h3>{userInfo?.birthday && `Birthday: ${userInfo?.birthday}`}</h3>
-          <h3>{userInfo?.hometown && `Hometown: ${userInfo?.hometown}`}</h3>
-          <h3>{userInfo?.medical_history && `Medical History: ${userInfo?.medical_history}`}</h3>
+          <div style={styles.leftContentContainer}>
+            <h3>User ID: {this.props.userInfo?.id}</h3>
+            <h3>Name: {`${userInfo?.first_name} ${userInfo?.last_name}`}</h3>
+            <h3>{userInfo?.email && `Email: ${userInfo?.email}`}</h3>
+            <h3>{userInfo?.phone && `Phone Number: ${userInfo?.phone}`}</h3>
+            <h3>{userInfo?.birthday && `Birthday: ${userInfo?.birthday}`}</h3>
+            <h3>{userInfo?.hometown && `Hometown: ${userInfo?.hometown}`}</h3>
+            <h3>{userInfo?.medical_history && `Medical History: ${userInfo?.medical_history}`}</h3>
 
-          { this.props.bodyParts && <h3>Number of Body Parts: {this.props.bodyParts.length}</h3> }
+            { this.props.bodyParts && <h3>Number of Body Parts: {this.props.bodyParts.length}</h3> }
 
-          { this.props.bodyParts && bodyParts.map((part) => {
-              if (part.stats?.num_entries > 0) {
-                return (
-                  <div key={part.id}>
-                    {(part.location && `${part.location} `)}
-                    {`${part.name} ${part.stats[this.state.statType]}`}
-                  </div>
-                );
-              }
-          })}
+            { this.props.bodyParts && bodyParts.map((part) => {
+                if (part.stats?.num_entries > 0) {
+                  return (
+                    <div key={part.id}>
+                      {(part.location && `${part.location} `)}
+                      {`${part.name} ${part.stats[this.state.statType]}`}
+                    </div>
+                  );
+                }
+            })}
 
-          <form>
-            <input name="statType" value={statTypes.avg} type="radio" onChange={this._handleInputChange}/> Avg
-            <input name="statType" value={statTypes.max} type="radio" onChange={this._handleInputChange}/> Max
-            <input name="statType" value={statTypes.min} type="radio" onChange={this._handleInputChange}/> Min
-          </form>
+            <form>
+              <input name="statType" value={statTypes.avg} type="radio" onChange={this._handleInputChange}/> Avg
+              <input name="statType" value={statTypes.max} type="radio" onChange={this._handleInputChange}/> Max
+              <input name="statType" value={statTypes.min} type="radio" onChange={this._handleInputChange}/> Min
+            </form>
+          </div>
+          <div style={styles.rightContentContainer}>
+            <div style={{ ...AppStyles.typContentContainer, flex: 1 }}>
+
+            </div>
+            <div style={{ ...AppStyles.typContentContainer, flex: 0.5, marginTop: 20 }}>
+
+            </div>
+          </div>
         </div>
 
       </div>
--- src/pages/Dashboard/style.js
+++ src/pages/Dashboard/style.js
@@ -7,9 +7,27 @@ import AppFonts from 'Common/AppFonts';
 export default {
     container: {
       ...AppStyles.fill,
-      backgroundColor: AppColors.lilac,
+      paddingTop: 80
     },
     contentContainer: {
-      paddingTop: 80
-    }
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      height: '100%'
+    },
+    leftContentContainer: {
+      flex: 1,
+      margin: 30,
+      backgroundColor: 'white',
+      padding: 30,
+      ...AppStyles.typContentContainer,
+    },
+    rightContentContainer: {
+      flex: 1,
+      margin: 30,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center'
+    },
+
 }
