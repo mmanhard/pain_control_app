19a4921e8cee775fd2dfa9b4b6cbb4782af1de69
Michael Manhard <mmanhard93@gmail.com>
2020-06-24 13:48:30 -0500

    -Add functionality to entries page. -Enable sorting based on date and pain levels. -Enable filtering based on date range, time of day, and body parts. -Add visualizer for each entry. -Show basic stats for each entry.

--- src/pages/Entries/index.js
+++ src/pages/Entries/index.js
@@ -1,48 +1,253 @@
 import React from "react";
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { withRouter } from 'react-router';
+import moment from 'moment';
 
 import actions from 'Actions';
 import Navbar from 'Components/Navbar';
+import BodyVisualizer from 'Components/BodyVisualizer';
 import styles from './style';
+
+const daytimes = {
+  all_day: 'All Day',
+  wakeup: 'Wake Up',
+  morning: 'Morning',
+  lunch: 'Lunch',
+  evening: 'Evening',
+  bed_time: 'Bed Time',
+};
+
+const dateRanges = {
+  all_time: 'All Time',
+  today: 'Today',
+  this_week: 'This Week',
+  this_month: 'This Month',
+  this_year: 'This Year',
+  custom: 'Custom'
+};
+
+const sortOptions = {
+  date: 'Date',
+  max_pain: 'Max Pain',
+  min_pain: 'Min Pain'
+}
 
 class Entries extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      start_date: null,
+      end_date: null,
+      currentBodyPart: null,
+      currentBodyPartID: null,
+      daytime: 'all_day',
+      sortBy: 'date',
     };
   }
 
   componentDidMount() {
+    this._reloadEntries();
+  }
+
+  _reloadEntries = () => {
     const { userInfo } = this.props;
-    this.props.getEntries(userInfo);
+    const { startDate, endDate, daytime, currentBodyPartID, sortBy } = this.state;
+
+    let params = { sort_by: sortBy, time_of_day: daytime };
+    if (currentBodyPartID) {
+      params = { ...params, pain_point: currentBodyPartID };
+    }
+    if (startDate) {
+      params = { ...params, start_date: startDate.toISOString() };
+    }
+    if (endDate) {
+      params = { ...params, end_date: endDate.toISOString() };
+    }
+    console.log(sortBy);
+
+    this.props.getEntries(userInfo, params);
+  }
+
+  _handleInputChange = (event) => {
+    const target = event.target;
+    this.setState({ [target.name]: target.value }, this._reloadEntries);
+  }
+
+  _handleDateRangeChange = (event) => {
+    const { userInfo } = this.props;
+    const target = event.target;
+
+    let startDate, endDate;
+    switch (target.value) {
+      case 'today':
+        startDate = moment().startOf('day');
+        break;
+      case 'this_week':
+        startDate = moment().subtract(6,'d').startOf('day');
+        break;
+      case 'this_month':
+        startDate = moment().subtract(1,'M').startOf('day');
+        break;
+      case 'this_year':
+        startDate = moment().subtract(1,'Y').startOf('day');
+        break;
+      case 'custom':
+        console.log('Need to handle custom case!!!');
+    }
+
+    this.setState({ startDate }, this._reloadEntries);
+  }
+
+  _handleSortChange = (sortBy) => {
+    this.setState({ sortBy }, this._reloadEntries);
+  }
+
+  _goToEntry = (entryID) => {
+    this.props.history.push(`entries/${entryID}`);
+  }
+
+  _renderEntry = (entry) => {
+    let visualizerBodyParts = entry.pain_subentries.map(subentry => {
+      const part = subentry.body_part;
+      const displayName = part.location ? `${part.location}_${part.name}` : part.name;
+      return ({
+        name: displayName,
+        id: part.id,
+        stats: subentry.pain_level
+      });
+    });
+
+    let min = 10;
+    let max = 0;
+    for (let subentry of entry.pain_subentries) {
+      if (subentry.pain_level < min) {
+        min = subentry.pain_level
+      }
+      if (subentry.pain_level > max) {
+        max = subentry.pain_level
+      }
+    }
+
+    const date = moment(entry.date).utc();
+    return (<div key={entry.id} style={styles.entryContainer}>
+      <div style={styles.entryDetailsContainer}>
+        <div style={styles.entryTitleContainer}>
+          <div>{date.format('MMMM Do, YYYY')}</div>
+          <div>{date.format('h:mm a')}</div>
+        </div>
+        <div style={styles.statsRow}>
+          <div style={styles.statContainer}>
+            <div style={styles.statTxt}>{max}</div>
+            <div style={styles.statTitle}>Max</div>
+          </div>
+          <div style={styles.statContainer}>
+            <div style={styles.statTxt}>{min}</div>
+            <div style={styles.statTitle}>Min</div>
+          </div>
+          <div style={styles.statContainer}>
+            <div style={styles.statTxt}>{entry.pain_subentries.length}</div>
+            <div style={styles.statTitle}># of Pain Points</div>
+          </div>
+        </div>
+        <button style={styles.continueBtn} onClick={() => { this._goToEntry(entry.id) }}>View Details</button>
+        <button style={styles.skipBtn} onClick={() => {  }}>Edit</button>
+      </div>
+      <BodyVisualizer
+        contentContainerStyle={styles.visualizer}
+        bodyParts={visualizerBodyParts} />
+    </div>);
   }
 
   render() {
     const { userInfo, bodyParts, entries, logout } = this.props;
+    const { sortBy } = this.state;
 
+    for (let entry of entries) {
+      console.log(entry.stats);
+    }
     return (
       <div style={styles.container}>
         <Navbar userInfo={userInfo} logout={logout}/>
-        <h2>Entries</h2>
-        <ul>
-          {entries.map((entry) => {
-            return (<li key={entry.id}>
-              <div>{`Entry ID: ${entry.id}`}</div>
-              <div>{`Entry Notes: ${entry.notes}`}</div>
-              <div>{`Entry Date: ${entry.date}`}</div>
-              <div>{`Entry Time: ${entry.time_of_day}`}</div>
-              <div>{`Max Pain: ${entry.stats.high}`}</div>
-              {entry.pain_subentries.map((subentry) => {
+        <div style={styles.configContainer}>
+          <div style={styles.titleContainer}>
+            <div style={styles.titleTxt}>My Entry Feed</div>
+            <div style={{...styles.subtitleTxt, marginTop: 8}}>Scroll down to see more entries.</div>
+            <div style={styles.subtitleTxt}>Add filters below.</div>
+          </div>
+          <div style={styles.configContentContainer}>
+            <div style={styles.configRow}>
+              <div style={styles.configTitle}>
+                <div style={styles.configTitleTxt}>Filter</div>
+                <div style={styles.configTitleTxt}>Entries</div>
+              </div>
+            </div>
+            <div style={styles.configRow}>
+              <div style={styles.filterOptions}>
 
-                return (<div key={subentry.body_part.id}>
-                  {`Body Part: ${subentry.body_part.name} - ${subentry.pain_level}`}
-                </div>);
-              })}
-            </li>);
-          })}
-        </ul>
+                <div style={styles.filterOption}>
+                  <div style={styles.filterOptionTitle}>Dates</div>
+                  <select name="dateRange" style={styles.filterOptionTxt} onChange={this._handleDateRangeChange}>
+                    {Object.entries(dateRanges).map(([key, value]) => {
+                      return (
+                        <option key={key} value={key}>{value}</option>
+                      );
+                    })}
+                  </select>
+                </div>
+
+                <div style={styles.filterOption}>
+                  <div style={styles.filterOptionTitle}>Pain Point</div>
+                  <select name="currentBodyPartID" style={styles.filterOptionTxt} onChange={this._handleInputChange}>
+                    <option key={'all_bps'} value={''}>All</option>
+                    {bodyParts.map((part) => {
+                      const displayName = part.location ? `${part.location} ${part.name}` : part.name;
+                      return (
+                        <option key={part.id} value={part.id}>{displayName}</option>
+                      );
+                    })}
+                  </select>
+                </div>
+
+                <div style={styles.filterOption}>
+                  <div style={styles.filterOptionTitle}>Time of Day</div>
+                  <select name="daytime" style={styles.filterOptionTxt} onChange={this._handleInputChange}>
+                    {Object.entries(daytimes).map(([key, value]) => {
+                      return (
+                        <option key={key} value={key}>{value}</option>
+                      );
+                    })}
+                  </select>
+                </div>
+
+              </div>
+            </div>
+            <div style={styles.configRow}>
+              <div style={styles.configTitle}>
+                <div style={styles.configTitleTxt}>Sort</div>
+                <div style={styles.configTitleTxt}>Entries</div>
+              </div>
+              <div style={styles.sortOptions}>
+                {Object.entries(sortOptions).map(([key, value]) => {
+                  const selected = (sortBy == key);
+                  return (
+                    <button
+                      style={styles.sortOption(selected)}
+                      key={key}
+                      onClick={() => { this._handleSortChange(key) }}>
+                      {value}
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+            <div style={styles.entryNumberText}># of Entries: {entries.length}</div>
+          </div>
+        </div>
+        <div style={styles.entriesContainer}>
+          {entries.map(this._renderEntry)}
+        </div>
       </div>
     )
   }
@@ -50,6 +255,7 @@ class Entries extends React.Component {
 
 const mapStateToProps = state => ({
   userInfo: state.users.userInfo,
+  bodyParts: state.users.bodyParts,
   entries: state.users.entries
 });
 
@@ -60,4 +266,4 @@ const mapDispatchToProps = dispatch => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Entries);
+)(withRouter(Entries));
--- src/pages/Entries/style.js
+++ src/pages/Entries/style.js
@@ -6,7 +6,181 @@ import AppFonts from 'Common/AppFonts';
 
 export default {
     container: {
-      ...AppStyles.fill,
-      paddingTop: 80
+      ...AppStyles.rowSpace,
+      padding: '110px 30px 30px 30px',
+    },
+    configContainer: {
+      width: 400,
+      marginRight: 30,
+      height: '100%',
+      ...AppStyles.columnStart
+    },
+    titleContainer: {
+      height: 150,
+      width: '100%',
+      backgroundColor: AppColors.blue,
+      ...AppFonts.Raleway.bold,
+      color: AppColors.white,
+      textAlign: 'center',
+    },
+    titleTxt: {
+      fontSize: AppFonts.size.xLarge,
+      textAlign: 'center',
+      marginTop: 32,
+    },
+    subtitleTxt: {
+      fontSize: AppFonts.size.medium,
+    },
+    configContentContainer: {
+      marginTop: 30,
+      height: 400,
+      width: 370,
+      ...AppStyles.typContentContainer,
+      ...AppStyles.columnSpace,
+      padding: '20px 10px 10px 20px',
+    },
+    configRow: {
+      ...AppStyles.rowStart,
+      width: '100%',
+    },
+    configTitle: {
+      ...AppStyles.center,
+      alignItems: 'flex-start',
+      width: 140,
+      height: 68,
+      borderRadius: 34,
+      backgroundColor: AppColors.blue,
+    },
+    configTitleTxt: {
+      marginLeft: 20,
+      color: AppColors.white,
+      fontSize: AppFonts.size.medLarge,
+    },
+    filterOptions: {
+      flex: 1,
+      ...AppStyles.rowCenter
+    },
+    filterOption: {
+      ...AppStyles.center,
+    },
+    filterOptionTitle: {
+      color: AppColors.blue,
+      ...AppFonts.Raleway.bold,
+      fontSize: AppFonts.size.medLarge,
+      margin: 10,
+    },
+    filterOptionTxt: {
+      height: 36,
+      width: 120,
+      borderRadius: 18,
+      backgroundColor: AppColors.lilac,
+      color: AppColors.blue,
+      ...AppFonts.Raleway.bold,
+      fontSize: AppFonts.size.medium,
+      textAlign: 'center',
+      border: 'none',
+      boxShadow: AppStyles.typBoxShadow,
+      alignItems: 'center'
+    },
+    sortOptions: {
+      flex: 1,
+      ...AppStyles.rowCenter,
+      flexWrap: 'wrap'
+    },
+    sortOption: (selected) => {
+      return {
+        textAlign: 'center',
+        ...AppFonts.Raleway.bold,
+        fontSize: AppFonts.size.medium,
+        border: 'none',
+        backgroundColor: selected ? AppColors.blue : AppColors.lilac,
+        color: selected ? AppColors.lilac : AppColors.blue,
+        borderRadius: 18,
+        width: 100,
+        height: 36,
+        marginLeft: 4,
+        marginRight: 4,
+        marginBottom: 16,
+        paddingLeft: 10,
+      };
+    },
+    entryNumberText: {
+      color: AppColors.blue,
+    },
+    entriesContainer: {
+      flex: 1,
+      marginLeft: 30,
+      marginRight: 30,
+      ...AppStyles.columnStart
+    },
+    entryContainer: {
+      width: '100%',
+      height: 350,
+      padding: 30,
+      marginBottom: 30,
+      ...AppStyles.typContentContainer,
+      ...AppStyles.rowStart,
+      alignItems: 'center'
+    },
+    entryDetailsContainer: {
+      flex: 1,
+      height: '100%',
+      ...AppStyles.columnStart,
+    },
+    entryTitleContainer: {
+      ...AppStyles.center,
+      alignItems: 'flex-start',
+      backgroundColor: AppColors.blue,
+      height: 110,
+      paddingLeft: 24,
+      paddingRight: 24,
+      fontSize: AppFonts.size.xLarge,
+      color: AppColors.white,
+      borderRadius: 20,
+      boxShadow: AppStyles.typBoxShadow
+    },
+    continueBtn: {
+      ...AppStyles.largeBtn,
+      backgroundColor: AppColors.blue,
+      color: AppColors.white,
+    },
+    skipBtn: {
+      ...AppStyles.subtleBtn,
+      marginTop: 10,
+    },
+    statsRow: {
+      ...AppStyles.rowStart,
+      alignItems: 'center',
+      flex: 1,
+      width: '95%',
+      marginTop: 10,
+    },
+    statContainer: {
+      ...AppStyles.center,
+      flex: 1,
+      textAlign: 'center',
+    },
+    statTxt: {
+      ...AppStyles.center,
+      backgroundColor: AppColors.blue,
+      height: 50,
+      width: 50,
+      borderRadius: 25,
+      color: AppColors.white
+    },
+    statTitle: {
+      marginTop: 8,
+      color: AppColors.blue,
+      fontSize: AppFonts.size.medSmall,
+    },
+    visualizer: {
+      backgroundColor: AppColors.white,
+      border: `solid 8px ${AppColors.blue}`,
+      boxShadow: AppStyles.typBoxShadow,
+      borderRadius: 40,
+      marginLeft: 50,
+      marginRight: 20,
+      height: '80%',
+      flex: 0.8
     },
 }
