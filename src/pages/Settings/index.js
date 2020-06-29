48c341e9d531404c2bf4307944442c69f4638735
Michael Manhard <mmanhard93@gmail.com>
2020-06-29 17:22:29 -0500

    -Add styling to settings page. -Add edit body part functionality to settings page.

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -7,21 +7,34 @@ import Navbar from 'Components/Navbar';
 import styles from './style';
 import withWindowDimensions from 'Common/AppDimens';
 import Utils from 'Utils';
+import AppColors from 'Common/AppColors';
 
 import BackIcon from 'Icons/icons8-back.png';
 import NameIcon from 'Icons/icons8-name.png';
 import PhoneIcon from 'Icons/icons8-phone.png';
 import BirthdayIcon from 'Icons/icons8-birthday.png';
 import HomeIcon from 'Icons/icons8-home.png';
+import KeyIcon from 'Icons/icons8-key.png';
+
+const screenTypes = {
+  editAccount: 'editAccount',
+  editPainPoints: 'editPainPoints',
+  editPassword: 'editPassword'
+}
 
 class Settings extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      currentBodyPart: null,
+      part_location: '',
+      part_name: '',
+      part_type: '',
       old_password: '',
       new_password_1: '',
-      new_password_2: ''
+      new_password_2: '',
+      screenType: screenTypes.editAccount
     };
   }
 
@@ -42,7 +55,7 @@ class Settings extends React.Component {
     this.setState({ birthday: Utils.formatDateInput(target.value) });
   };
 
-  _handleSubmitInfo = () => {
+  _handleEditAccount = () => {
     const { userInfo } = this.props;
     const { first_name, last_name, phone, birthday, hometown } = this.state;
     let userUpdates = {};
@@ -55,6 +68,29 @@ class Settings extends React.Component {
     if (hometown?.length > 0) userUpdates.hometown = hometown;
 
     this.props.updateUser(userInfo, userUpdates);
+  }
+
+  _handleEditBodyPart = () => {
+    const { userInfo } = this.props;
+    const { currentBodyPart: part, part_name, part_location, part_type } = this.state;
+
+    let partUpdates = {};
+    if (part_name?.length > 0) partUpdates.name = part_name;
+    if (part_location?.length > 0) partUpdates.location = part_location;
+    if (part_type?.length > 0) partUpdates.type = part_type;
+
+    this.props.editBodyPart(userInfo, part.id, partUpdates);
+    this.setState({ currentBodyPart: null })
+  }
+
+  _handleAddBodyPart = () => {
+    const { userInfo } = this.props;
+    const { part_name, part_location, part_type } = this.state;
+
+    if (part_name?.length > 0 && part_location?.length > 0 && part_type?.length > 0) {
+      const partInfo = { name: part_name, location: part_location, type: part_type }
+      this.props.addBodyPart(userInfo, partInfo);
+    }
   }
 
   _handleChangePassword = () => {
@@ -72,15 +108,17 @@ class Settings extends React.Component {
   _renderEditAccount = () => {
     const { userInfo } = this.props;
     return (
-      <div style={styles.entryContainer}>
-        <div style={styles.entryTitleContainer}>
-          <div>Account</div>
-          <div>Settings</div>
+      <div style={styles.editAccountContainer}>
+        <div style={styles.editAccountLeft}>
+          <div style={styles.settingsTitleContainer}>
+            <div>Account</div>
+            <div>Settings</div>
+          </div>
+          <button style={{position: 'absolute', left: '25%', top: '60%', ...styles.continueBtn}} onClick={this._handleEditAccount}>Submit</button>
         </div>
-        <button onClick={this._handleSubmitInfo}>Submit</button>
         <div style={styles.formContainer}>
           <div style={styles.txtInputContainer}>
-            <img src={NameIcon} style={{width: 24, height: 24, margin: 'auto'}} />
+            <img src={NameIcon} style={{height: 24, margin: 'auto'}} />
             <input
               name="first_name"
               style={styles.txtInput}
@@ -136,27 +174,137 @@ class Settings extends React.Component {
     );
   }
 
-  _renderEditBodyParts = () => {
+  _renderPartsRow = (parts, offset = 0) => {
+    const { currentBodyPart, addMoreParts } = this.state;
     return (
-      <div style={styles.entryContainer}>
-        <div style={styles.entryTitleContainer}>
-          <div>Edit Pain</div>
-          <div>Points</div>
+      <div style={styles.partsContainer(offset)}>
+        {parts.map((part) => {
+          const selected = currentBodyPart && currentBodyPart.id == part.id;
+          const displayName = part.location ? `${part.location} ${part.name}` : part.name;
+          return (
+            <button
+              key={part.id}
+              onClick={() => { this.setState({ currentBodyPart: part, addMoreParts: false, part_name: '', part_location: '', part_type: '' })}}
+              style={styles.partContainer(selected)}>
+              <div>{displayName}</div>
+              <div style={styles.editTxt}>Edit</div>
+            </button>
+          );
+        })}
+        {parts.length < 3 && (
+          <button
+            style={styles.addMorePartsBtn(addMoreParts)}
+            onClick={() => {this.setState({ currentBodyPart: null, addMoreParts: true, part_name: '', part_location: '', part_type: '' })}}>
+              Add More
+          </button>
+        )}
+      </div>
+    );
+  }
+
+  _renderEditBodyParts = () => {
+    const { bodyParts } = this.props;
+    const { currentBodyPart: part, addMoreParts } = this.state;
+
+    return (
+      <div style={styles.editPartsContainer}>
+        <div style={{width: '100%', position: 'relative', height: 110}}>
+          <div style={styles.settingsTitleContainer}>
+            <div>Edit Pain</div>
+            <div>Points</div>
+          </div>
         </div>
+        <div style={styles.bodyPartsContainer}>
+          {this._renderPartsRow(bodyParts.slice(0,3), 30)}
+          {this._renderPartsRow(bodyParts.slice(3,6), -30)}
+          {this._renderPartsRow(bodyParts.slice(6,9), 30)}
+          {this._renderPartsRow(bodyParts.slice(9,12), -30)}
+        </div>
+        {part && (
+          <div style={styles.partDetailsContainer}>
+            <div style={styles.partInputContainer}>
+              <input
+                name="part_location"
+                style={styles.txtInput}
+                placeholder='Location'
+                type="text"
+                value={this.state.part_location ? this.state.part_location : part.location}
+                onChange={this._handleInputChange}
+              />
+            </div>
+            <div style={styles.partInputContainer}>
+              <input
+                name="part_name"
+                style={styles.txtInput}
+                placeholder='Name'
+                type="text"
+                value={this.state.part_name ? this.state.part_name : part.name}
+                onChange={this._handleInputChange}
+              />
+            </div>
+            <div style={styles.partInputContainer}>
+              <input
+                name="part_type"
+                style={styles.txtInput}
+                placeholder='Type'
+                type="text"
+                value={this.state.part_type ? this.state.part_type : part.type}
+                onChange={this._handleInputChange}
+              />
+            </div>
+            <button style={styles.continueBtn} onClick={this._handleEditBodyPart}>Submit</button>
+          </div>)}
+          {addMoreParts && (
+            <div style={styles.partDetailsContainer}>
+              <div style={styles.partInputContainer}>
+                <input
+                  name="part_location"
+                  style={styles.txtInput}
+                  placeholder='Location'
+                  type="text"
+                  value={this.state.part_location}
+                  onChange={this._handleInputChange}
+                />
+              </div>
+              <div style={styles.partInputContainer}>
+                <input
+                  name="part_name"
+                  style={styles.txtInput}
+                  placeholder='Name'
+                  type="text"
+                  value={this.state.part_name}
+                  onChange={this._handleInputChange}
+                />
+              </div>
+              <div style={styles.partInputContainer}>
+                <input
+                  name="part_type"
+                  style={styles.txtInput}
+                  placeholder='Type'
+                  type="text"
+                  value={this.state.part_type}
+                  onChange={this._handleInputChange}
+                />
+              </div>
+              <button style={styles.continueBtn} onClick={this._handleAddBodyPart}>Submit</button>
+            </div>)}
       </div>
     );
   }
 
   _renderChangePassword = () => {
     return (
-      <div style={styles.entryContainer}>
-        <div style={styles.entryTitleContainer}>
-          <div>Change</div>
-          <div>Password</div>
+      <div style={styles.editPwdContainer}>
+        <div style={styles.changePwdLeft}>
+          <div style={styles.settingsTitleContainer}>
+            <div>Change</div>
+            <div>Password</div>
+          </div>
+          <button style={{position: 'absolute', left: '25%', top: '60%', ...styles.continueBtn}} onClick={this._handleChangePassword}>Submit</button>
         </div>
         <div style={styles.formContainer}>
           <div style={{...styles.txtInputContainer}}>
-            <img src={PhoneIcon} style={{height: 24, margin: 'auto' }} />
+            <img src={KeyIcon} style={{height: 24, margin: 'auto' }} />
             <input
               name="old_password"
               style={styles.txtInput}
@@ -166,29 +314,28 @@ class Settings extends React.Component {
               onChange={this._handleInputChange}
             />
           </div>
-          <div style={{...styles.txtInputContainer}}>
-            <img src={PhoneIcon} style={{height: 24, margin: 'auto' }} />
+          <div style={{...styles.txtInputContainer, border: 'none'}}>
+            <div style={{width: 24, height: 24, margin: 'auto' }} />
             <input
               name="new_password_1"
-              style={styles.txtInput}
+              style={{...styles.txtInput, borderBottom: `1px solid ${AppColors.blue}`}}
               placeholder='New Password'
               type="password"
               value={this.state.new_password_1}
               onChange={this._handleInputChange}
             />
           </div>
-          <div style={{...styles.txtInputContainer}}>
-            <img src={PhoneIcon} style={{height: 24, margin: 'auto' }} />
+          <div style={{...styles.txtInputContainer, border: 'none'}}>
+            <div style={{width: 24, height: 24, margin: 'auto' }} />
             <input
               name="new_password_2"
-              style={styles.txtInput}
-              placeholder='New Password'
+              style={{...styles.txtInput, borderBottom: `1px solid ${AppColors.blue}`}}
+              placeholder='Re-enter New Password'
               type="password"
               value={this.state.new_password_2}
               onChange={this._handleInputChange}
             />
           </div>
-          <button onClick={this._handleChangePassword}>Submit</button>
         </div>
       </div>
     );
@@ -196,6 +343,11 @@ class Settings extends React.Component {
 
   render() {
     const { userInfo, windowWidth, logout } = this.props;
+    const { screenType } = this.state;
+
+    const showAccount = screenType == screenTypes.editAccount;
+    const showPainPoints = screenType == screenTypes.editPainPoints;
+    const showPassword = screenType == screenTypes.editPassword;
 
     return (
       <div style={styles.container}>
@@ -206,24 +358,30 @@ class Settings extends React.Component {
             <div style={{...styles.subtitleTxt, marginTop: 8}}>Modify your account here.</div>
           </div>
           <div style={styles.configContentContainer}>
-            <div style={styles.configTitle}>
-              <div style={styles.configTitleTxt}>Filter</div>
-              <div style={styles.configTitleTxt}>Entries</div>
-            </div>
-            <div style={styles.configTitle}>
-              <div style={styles.configTitleTxt}>Filter</div>
-              <div style={styles.configTitleTxt}>Entries</div>
-            </div>
-            <div style={styles.configTitle}>
-              <div style={styles.configTitleTxt}>Filter</div>
-              <div style={styles.configTitleTxt}>Entries</div>
-            </div>
+            <button
+              onClick={() => {this.setState({ screenType: screenTypes.editAccount })}}
+              style={styles.configTitle(showAccount)}>
+              <div style={styles.configTitleTxt}>Account</div>
+              <div style={styles.configTitleTxt}>Settings</div>
+            </button>
+            <button
+              onClick={() => {this.setState({ screenType: screenTypes.editPainPoints })}}
+              style={styles.configTitle(showPainPoints)}>
+              <div style={styles.configTitleTxt}>Edit</div>
+              <div style={styles.configTitleTxt}>Pain Points</div>
+            </button>
+            <button
+              onClick={() => {this.setState({ screenType: screenTypes.editPassword })}}
+              style={styles.configTitle(showPassword)}>
+              <div style={styles.configTitleTxt}>Change</div>
+              <div style={styles.configTitleTxt}>Password</div>
+            </button>
           </div>
         </div>
         <div style={styles.entriesContainer(windowWidth)}>
-          {this._renderEditAccount()}
-          {this._renderEditBodyParts()}
-          {this._renderChangePassword()}
+          {showAccount && this._renderEditAccount()}
+          {showPainPoints && this._renderEditBodyParts()}
+          {showPassword && this._renderChangePassword()}
         </div>
       </div>
     )
@@ -232,6 +390,7 @@ class Settings extends React.Component {
 
 const mapStateToProps = state => ({
   userInfo: state.users.userInfo,
+  bodyParts: state.users.bodyParts
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/pages/Settings/style.js
+++ src/pages/Settings/style.js
@@ -28,33 +28,39 @@ export default {
   titleTxt: {
     fontSize: AppFonts.size.xLarge,
     textAlign: 'center',
-    marginTop: 32,
+    marginTop: 40,
   },
   subtitleTxt: {
     fontSize: AppFonts.size.medium,
   },
   configContentContainer: {
     marginTop: 30,
-    height: 400,
-    width: 370,
+    height: 200,
+    width: 400,
     ...AppStyles.typContentContainer,
     ...AppStyles.rowCenter,
     alignItems: 'center',
+    alignContent: 'center',
     flexWrap: 'wrap',
-    padding: '20px 10px 10px 20px',
   },
-  configTitle: {
-    ...AppStyles.center,
-    alignItems: 'flex-start',
-    width: 140,
-    height: 68,
-    borderRadius: 34,
-    backgroundColor: AppColors.blue,
+  configTitle: (selected) => {
+    return {
+      ...AppStyles.center,
+      alignItems: 'flex-start',
+      width: 140,
+      height: 68,
+      borderRadius: 34,
+      margin: 10,
+      color: selected ? AppColors.white : AppColors.blue,
+      backgroundColor: selected ? AppColors.blue : AppColors.lilac,
+      border: 'none',
+      boxShadow: AppStyles.typBoxShadow,
+      fontSize: AppFonts.size.medLarge,
+      ...AppFonts.Raleway.bold,
+    }
   },
   configTitleTxt: {
     marginLeft: 20,
-    color: AppColors.white,
-    fontSize: AppFonts.size.medLarge,
   },
   entriesContainer: (windowWidth) => {
     return {
@@ -67,22 +73,113 @@ export default {
       ...AppStyles.columnStart
     }
   },
-  entryContainer: {
+  editAccountContainer: {
     width: '100%',
-    height: 350,
     padding: 30,
     marginBottom: 30,
     ...AppStyles.typContentContainer,
     ...AppStyles.rowStart,
     alignItems: 'center'
   },
+  editPartsContainer: {
+    width: '100%',
+    padding: 30,
+    marginBottom: 30,
+    ...AppStyles.typContentContainer,
+    ...AppStyles.center
+  },
+  editPwdContainer: {
+    width: '100%',
+    padding: 30,
+    marginBottom: 30,
+    ...AppStyles.typContentContainer,
+    ...AppStyles.rowStart,
+    alignItems: 'center'
+  },
+  bodyPartsContainer: {
+    width: '100%',
+    marginTop: 30,
+  },
+  partsContainer: (offset) => {
+    return {
+      marginLeft: offset,
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center'
+    };
+  },
+  partContainer: (selected) => {
+    return {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      ...AppFonts.Raleway.bold,
+      fontSize: AppFonts.size.medium,
+      border: 'none',
+      backgroundColor: selected ? AppColors.blue : AppColors.lilac,
+      color: selected ? AppColors.lilac : AppColors.blue,
+      borderRadius: 18,
+      width: 140,
+      height: 36,
+      marginLeft: 4,
+      marginRight: 4,
+      marginBottom: 16,
+      paddingLeft: 10,
+    };
+  },
+  addMorePartsBtn: (selected) => {
+    return {
+      ...AppFonts.Raleway.bold,
+      fontSize: AppFonts.size.medium,
+      border: 'none',
+      backgroundColor: selected ? AppColors.blue : AppColors.lilac,
+      color: selected ? AppColors.lilac : AppColors.blue,
+      borderRadius: 18,
+      width: 140,
+      height: 36,
+      marginLeft: 4,
+      marginRight: 4,
+      marginBottom: 16,
+    };
+  },
+  partDetailsContainer: {
+    marginTop: 20,
+    flex: 1,
+    width: '100%',
+    ...AppStyles.rowCenter
+  },
+  editTxt: {
+    margin: 6,
+    height: 24,
+    width: 40,
+    borderRadius: 14,
+    backgroundColor: AppColors.white,
+    color: AppColors.black,
+    ...AppStyles.center,
+    textAlign: 'center',
+    border: 'none',
+    fontSize: AppFonts.size.medSmall
+  },
+  editAccountLeft: {
+    flex: 1,
+    height: '100%',
+    position: 'relative',
+  },
+  changePwdLeft: {
+    flex: 1,
+    height: '100%',
+    position: 'relative',
+  },
   formContainer: {
     ...AppStyles.center,
-    width: '100%'
+    flex: 2,
+    marginTop: 30,
   },
   txtInputContainer: {
     height: 36,
-    marginBottom: 60,
+    marginBottom: 40,
+    maxWidth: 320,
     width: '65%',
     display: 'flex',
     flexDirection: 'row',
@@ -95,17 +192,32 @@ export default {
     paddingLeft: 10,
     border: 'none',
     backgroundColor: 'transparent',
+    width: 50,
+  },
+  partInputContainer: {
+    height: 36,
+    marginBottom: 40,
+    marginRight: 20,
+    width: 120,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    borderBottom: `1px solid ${AppColors.blue}`
   },
   entryDetailsContainer: {
     flex: 1,
     height: '100%',
     ...AppStyles.columnStart,
   },
-  entryTitleContainer: {
+  settingsTitleContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: 110,
+    width: 200,
     ...AppStyles.center,
     alignItems: 'flex-start',
     backgroundColor: AppColors.blue,
-    height: 110,
     paddingLeft: 24,
     paddingRight: 24,
     fontSize: AppFonts.size.xLarge,
