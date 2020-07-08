6600f6720f3d7d34e4fd87d8354339771c113e4d
Michael Manhard <mmanhard93@gmail.com>
2020-07-08 17:20:32 -0500

    -Add functionality to pain point detail screen including charts and filtering. -Add styling to pain point detail screen.

--- src/pages/PainPointDetail/index.js
+++ src/pages/PainPointDetail/index.js
@@ -1,16 +1,63 @@
 import React from "react";
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import moment from 'moment';
 
 import actions from 'Actions';
 import Navbar from 'Components/Navbar';
 import styles from './style';
+import DaytimeChart from 'Components/DaytimeChart';
+import CalendarChart from 'Components/CalendarChart';
+import HistogramChart from 'Components/HistogramChart';
+import BodyVisualizer from 'Components/BodyVisualizer';
+import Utils from 'Utils';
+import AppStyles from 'Common/AppStyles';
+
+const chartTypes = {
+  daytime: 'daytime',
+  calendar: 'calendar',
+  histogram: 'histogram'
+}
+
+const daytimeStatTypes = {
+  mean: 'Mean',
+  median: 'Median',
+  high: 'Max',
+  low: 'Min',
+  stdev: 'Std Dev'
+}
+
+const calendarStatTypes = {
+  mean: 'Mean',
+  high: 'Max',
+  low: 'Min',
+  stdev: 'Std Dev'
+}
+
+const histogramStatTypes = {
+  mean: 'Mean',
+  median: 'Median',
+  high: 'Max',
+  low: 'Min',
+}
+
+const dateRanges = {
+  all_time: 'All Time',
+  this_week: 'This Week',
+  this_month: 'This Month',
+  this_year: 'This Year',
+  custom: 'Custom'
+};
 
 class PainPointDetail extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      chartType: chartTypes.calendar,
+      statType: 'mean',
+      customStartDate: undefined,
+      customEndDate: undefined,
     };
   }
 
@@ -19,21 +66,207 @@ class PainPointDetail extends React.Component {
     this.props.getBodyPart(userInfo, bodyPartID, {});
   }
 
+  _handleInputChange = (event) => {
+    const target = event.target;
+    this.setState({ [target.name]: target.value });
+  }
+
+  _handleDateRangeChange = (event) => {
+    const { userInfo, bodyPartID } = this.props;
+    const target = event.target;
+
+    let startDate, endDate;
+    switch (target.value) {
+      case 'today':
+        startDate = moment().startOf('day');
+        break;
+      case 'this_week':
+        startDate = moment().subtract(6,'d').startOf('day');
+        break;
+      case 'this_month':
+        startDate = moment().subtract(1,'M').startOf('day');
+        break;
+      case 'this_year':
+        startDate = moment().subtract(1,'Y').startOf('day');
+        break;
+      case 'custom':
+        startDate = moment().subtract(1,'Y').startOf('day');
+        endDate = moment();
+        this.setState({ customStartDate: startDate.format('MM/DD/YYYY'), customEndDate: endDate.format('MM/DD/YYYY') });
+    }
+
+    if (target.value !== 'custom') {
+      this.setState({ customStartDate: undefined, customEndDate: undefined });
+    }
+
+    let params = {};
+    if (startDate) {
+      params = { ...params, start_date: startDate.toISOString() };
+    }
+    if (endDate) {
+      params = { ...params, end_date: endDate.toISOString() };
+    }
+    this.props.getBodyPart(userInfo, bodyPartID, params);
+  }
+
+  _handleCustomDateChange = (event) => {
+    const target = event.target;
+    const { entryDate, entryTime, entryTimePeriod } = this.state;
+
+    this.setState({ [target.name]: Utils.formatDateInput(target.value) });
+  }
+
+  _handleSubmitCustomDates = () => {
+    const { userInfo, bodyPartID } = this.props;
+    const { customStartDate, customEndDate } = this.state;
+
+    const startDate = Utils.convertDateTimeToMoment(customStartDate, '12:00', 'AM');
+    const endDate = Utils.convertDateTimeToMoment(customEndDate, '11:59', 'PM');
+
+    if (startDate && endDate) {
+      const params = { start_date: startDate.toISOString(), end_date: endDate.toISOString() };
+      this.props.getBodyPart(userInfo, bodyPartID, params);
+    } else{
+      alert('Incomplete dates!')
+    }
+  }
+
+  _renderFilterContainer = () => {
+    const { chartType, customStartDate, customEndDate } = this.state;
+
+    let currentStatTypes;
+    switch (chartType) {
+      case chartTypes.daytime:
+        currentStatTypes = daytimeStatTypes;
+        break;
+      case chartTypes.calendar:
+        currentStatTypes = calendarStatTypes;
+        break;
+      case chartTypes.histogram:
+        currentStatTypes = histogramStatTypes;
+        break;
+    }
+    return (
+      <div style={styles.filterContainer(customStartDate)}>
+        <div style={styles.filterTxt}>Show:</div>
+        <select name="statType" style={styles.filterOptionTxt} onChange={this._handleInputChange}>
+          {Object.entries(currentStatTypes).map(([key, value]) => {
+            return (
+              <option key={key} value={key}>{value}</option>
+            );
+          })}
+        </select>
+        <select name="dateRange" style={styles.filterOptionTxt} onChange={this._handleDateRangeChange}>
+          {Object.entries(dateRanges).map(([key, value]) => {
+            return (
+              <option key={key} value={key}>{value}</option>
+            );
+          })}
+        </select>
+        {(typeof customStartDate !== 'undefined') && <div style={AppStyles.center}>
+          <div style={styles.filterTxt}>Start Date</div>
+          <input
+            type='text'
+            style={styles.configTimeTxt}
+            name='customStartDate'
+            value={customStartDate}
+            onChange={this._handleCustomDateChange}
+          />
+          <div style={styles.filterTxt}>End Date</div>
+          <input
+            type='text'
+            style={styles.configTimeTxt}
+            name='customEndDate'
+            value={customEndDate}
+            onChange={this._handleCustomDateChange}
+          />
+          <button
+            style={styles.submitDateBtn}
+            onClick={this._handleSubmitCustomDates}>
+            Submit
+          </button>
+        </div>}
+      </div>
+    );
+  }
+
   render() {
     const { userInfo, bodyPartInfo, logout } = this.props;
+    const { chartType, statType } = this.state;
+
+    let daytimeStats = bodyPartInfo.stats.daytime;
+    let calendarStats = bodyPartInfo.stats.calendar;
+    let movingStats = bodyPartInfo.stats.moving;
+    let histogram =  bodyPartInfo.stats.histogram;
+
+    const displayName = bodyPartInfo.location ? `${bodyPartInfo.location} ${bodyPartInfo.name}` : bodyPartInfo.name;
+    const visualizerName = bodyPartInfo.location ? `${bodyPartInfo.location}_${bodyPartInfo.name}` : bodyPartInfo.name;
 
     return (
       <div style={styles.container}>
         <Navbar userInfo={userInfo} logout={logout}/>
-        <h2>PainPointDetail</h2>
-        <h3>{`Pain Point ID: ${bodyPartInfo?.id}`}</h3>
-        <h3>{`Pain Point Name: ${bodyPartInfo?.name}`}</h3>
-        <h3>{`Pain Point Location: ${bodyPartInfo?.location}`}</h3>
-        <h3>{`Pain Max: ${bodyPartInfo?.customStats?.total?.high}`}</h3>
-        <h3>{`Pain Min: ${bodyPartInfo?.customStats?.total?.low}`}</h3>
-        <h3>{`Pain Mean: ${bodyPartInfo?.customStats?.total?.mean}`}</h3>
-        <h3>{`Pain Median: ${bodyPartInfo?.customStats?.total?.median}`}</h3>
-        <h3>{`Pain StdDev: ${bodyPartInfo?.customStats?.total?.stdev}`}</h3>
+        <div style={styles.configContainer}>
+          <div style={styles.titleContainer}>
+            <div style={styles.titleTxt}>My {displayName}</div>
+            {true && <BodyVisualizer
+              clickBodyPartFound={() => {}}
+              clickBodyPartNotFound={() => {}}
+              contentContainerStyle={styles.miniVisualizer}
+              bodyParts={[{name: visualizerName, stats: 1}]}/>}
+          </div>
+          <div style={styles.configContentContainer}>
+            <div style={styles.toggleTxtContainer}>
+              <div>Toggle</div>
+              <div>Chart Type</div>
+            </div>
+            <button
+              style={chartType === chartTypes.daytime ? styles.mainButton : styles.mainButtonInactive}
+              onClick={() => {this.setState({ statType: 'mean', chartType: chartTypes.daytime })}}>
+              Time of Day
+            </button>
+            <button
+              style={chartType === chartTypes.calendar ? styles.mainButton : styles.mainButtonInactive}
+              onClick={() => {this.setState({ statType: 'mean', chartType: chartTypes.calendar })}}>
+              Pain Over Time
+            </button>
+            <button
+              style={chartType === chartTypes.histogram ? styles.mainButton : styles.mainButtonInactive}
+              onClick={() => {this.setState({ statType: 'mean', chartType: chartTypes.histogram })}}>
+              Histogram
+            </button>
+          </div>
+        </div>
+        <div style={styles.mainContentContainer}>
+          <div style={styles.graphContainer}>
+            <div style={styles.graphTitle}>
+              {chartType === chartTypes.daytime && 'Pain Throughout the Day'}
+              {chartType === chartTypes.calendar && 'Pain Over Time'}
+              {chartType === chartTypes.histogram && 'Frequency of Pain'}
+            </div>
+            {chartType === chartTypes.daytime &&
+              <DaytimeChart
+                graphStyle={styles.graph}
+                daytimeDatasets={[daytimeStats]}
+                statType={statType} />}
+
+            {chartType === chartTypes.calendar &&
+              <CalendarChart
+                graphStyle={styles.graph}
+                movingStats={[...movingStats].reverse()}
+                calendarStats={[...calendarStats].reverse()}
+                statType={statType} />}
+
+            {chartType === chartTypes.histogram &&
+              <HistogramChart
+                graphStyle={styles.graph}
+                histograms={[histogram]}
+                statType={statType} />}
+          </div>
+
+          {this._renderFilterContainer()}
+
+        </div>
+
       </div>
     )
   }
--- src/pages/PainPointDetail/style.js
+++ src/pages/PainPointDetail/style.js
@@ -5,8 +5,180 @@ import AppFonts from 'Common/AppFonts';
 
 
 export default {
-    container: {
-      ...AppStyles.fill,
-      paddingTop: 80
-    },
+  container: {
+    padding: '110px 30px 30px 30px',
+    ...AppStyles.columnStart,
+    flex: 1,
+  },
+  configContainer: {
+    width: '100%',
+    top: 110,
+    left: 30,
+    marginRight: 30,
+    height: 150,
+    marginBottom: 30,
+    ...AppStyles.rowSpace,
+  },
+  titleContainer: {
+    height: '100%',
+    width: 400,
+    backgroundColor: AppColors.blue,
+    ...AppFonts.Raleway.bold,
+    color: AppColors.white,
+    textAlign: 'center',
+    marginRight: 30,
+    ...AppStyles.rowSpace,
+    alignItems: 'center'
+  },
+  titleTxt: {
+    fontSize: AppFonts.size.xxLarge,
+    textAlign: 'center',
+    flex: 1,
+  },
+  subtitleTxt: {
+    fontSize: AppFonts.size.medium,
+  },
+  configContentContainer: {
+    flex: 1,
+    minWidth: 400,
+    height: '100%',
+    ...AppStyles.typContentContainer,
+    ...AppStyles.rowSpace,
+    alignItems: 'center'
+  },
+  toggleTxtContainer: {
+    height: 90,
+    width: 100,
+    borderRadius: 20,
+    backgroundColor: AppColors.lilac,
+    color: AppColors.blue,
+    marginLeft: 20,
+    paddingLeft: 20,
+    ...AppStyles.center,
+    alignItems: 'start'
+  },
+  mainButtonInactive: {
+    ...AppFonts.Raleway.bold,
+    fontSize: AppFonts.size.medLarge,
+    border: 'none',
+    boxShadow: `0px 2px 2px rgba(0,0,0,0.15)`,
+    height: 60,
+    width: 180,
+    borderRadius: 30,
+    color: AppColors.blue,
+    backgroundColor: AppColors.lilac,
+    margin: 10,
+  },
+  mainButton: {
+    ...AppFonts.Raleway.bold,
+    fontSize: AppFonts.size.medLarge,
+    border: 'none',
+    boxShadow: `0px 2px 2px rgba(0,0,0,0.15)`,
+    height: 60,
+    width: 180,
+    borderRadius: 30,
+    color: AppColors.white,
+    backgroundColor: AppColors.blue,
+    margin: 10,
+  },
+  mainContentContainer: {
+    ...AppStyles.typContentContainer,
+    padding: 20,
+    flex: 1,
+    width: '90%',
+    ...AppStyles.rowSpace
+  },
+  graphContainer: {
+    ...AppStyles.center,
+    flex: 1,
+    height: '100%',
+    position: 'relative',
+  },
+  graphTitle: {
+    backgroundColor: AppColors.blue,
+    color: AppColors.white,
+    fontSize: AppFonts.size.large,
+    position: 'relative',
+    left: '5%',
+    paddingLeft: 24,
+    paddingRight: 24,
+    paddingTop: 12,
+    height: 48,
+    borderRadius: 30,
+    marginBottom: 20,
+  },
+  graph: {
+    backgroundColor: AppColors.white,
+    overflow: 'visible'
+  },
+  filterContainer: (customStartDate) => {
+    return {
+      ...AppStyles.center,
+      justifyContent: 'space-evenly',
+      borderRadius: 20,
+      backgroundColor: AppColors.lilac,
+      width: 180,
+      height: customStartDate ? 400 : 200,
+    };
+  },
+  filterTxt: {
+    color: AppColors.lightGrey,
+    textDecoration: 'underline',
+    margin: 8,
+  },
+  configTimeTxt: {
+    background: 'none',
+    color: AppColors.black,
+    border: 'none',
+    textAlign: 'center',
+    width: 120,
+    margin: 10,
+    paddingBottom: 8,
+    borderBottom: `solid 2px ${AppColors.blue}`,
+    ...AppFonts.Raleway.bold,
+    fontSize: AppFonts.size.medium,
+  },
+  filterOptionTxt: {
+    height: 36,
+    width: 120,
+    borderRadius: 18,
+    backgroundColor: AppColors.white,
+    color: AppColors.blue,
+    ...AppFonts.Raleway.bold,
+    fontSize: AppFonts.size.medium,
+    textAlign: 'center',
+    border: 'none',
+    boxShadow: AppStyles.typBoxShadow,
+    alignItems: 'center',
+    margin: 8,
+  },
+  filterDateContainer: {
+    ...AppStyles.rowCenter,
+    justifyContent: 'space-evenly',
+    width: '100%'
+  },
+  filterDate: {
+    textDecoration: 'underline'
+  },
+  submitDateBtn: {
+    backgroundColor: AppColors.blue,
+    height: 32,
+    width: 80,
+    borderRadius: 16,
+    textAlign: 'center',
+    border: 'none',
+    color: AppColors.white,
+    ...AppFonts.Raleway.bold,
+    marginBottom: 12,
+    boxShadow: AppStyles.typBoxShadow
+  },
+  miniVisualizer: {
+    height: 110,
+    width: 80,
+    border: `solid 4px ${AppColors.blue}`,
+    backgroundColor: AppColors.white,
+    boxShadow: AppStyles.typBoxShadow,
+    borderRadius: 12,
+    marginRight: 30,
+  }
 }
