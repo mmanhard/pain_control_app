b8444316066136e07ee8bf27413583d199d6f993
Michael Manhard <mmanhard93@gmail.com>
2020-07-15 16:35:29 -0500

    -Add categories for window sizes. -Fix window width / height scaling.

--- src/common/AppDimens.js
+++ src/common/AppDimens.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
 
+const screenSizes = {
+  xSmall: 576,
+  small: 768,
+  medium: 992,
+  large: 1200
+}
+
 export default function withWindowDimensions(WrappedComponent) {
     return class extends Component {
         state = { width: 0, height: 0 };
@@ -14,13 +21,19 @@ export default function withWindowDimensions(WrappedComponent) {
         }
 
         updateWindowDimensions = () => {
-            this.setState({ width: window.innerWidth, height: window.innerHeight });
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+            this.setState({ width, height });
         };
 
         render() {
             return (
                 <WrappedComponent
                     {...this.props}
+                    isMobile={this.state.width < screenSizes.xSmall}
+                    isSmallScreen={this.state.width < screenSizes.small}
+                    isMediumScreen={this.state.width < screenSizes.medium}
+                    isLargeScreen={this.state.width < screenSizes.large}
                     windowWidth={this.state.width}
                     windowHeight={this.state.height}
                 />
