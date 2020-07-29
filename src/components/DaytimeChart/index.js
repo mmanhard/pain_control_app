3c07aa9b02e89ca54938d0221c9bd68e065c7151
Michael Manhard <mmanhard93@gmail.com>
2020-07-29 17:04:40 -0500

    -Modify daytime chart styles.

--- src/components/DaytimeChart/index.js
+++ src/components/DaytimeChart/index.js
@@ -1,10 +1,9 @@
 import React from "react";
+import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Label, Tooltip } from 'recharts';
 
-// import styles from './style';
 import AppColors from 'Common/AppColors';
 import AppStyles from 'Common/AppStyles';
-
-import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Label, Tooltip } from 'recharts';
+import RotatedAxisTick from 'Components/RotatedAxisTick';
 
 const daytimeOrder = {
   sleep: 0,
@@ -61,17 +60,20 @@ class DaytimeChart extends React.Component {
 
     return (
       <div style={AppStyles.rowCenter}>
-        <div style={{...AppStyles.center, height: height}}>
+        <div style={{...AppStyles.center, height: (height-66), alignSelf: 'flex-start'}}>
           <div style={styles.yLabel}>{statDisplayName} Pain Level</div>
         </div>
-        <BarChart width={width} height={height} data={data} margin={{ top: 8, right: 0, bottom: 8, left: 0 }}>
+        <BarChart width={width} height={height} data={data} margin={{ top: 8, right: 0, bottom: 32, left: -30 }}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis tick={{fontSize}} dataKey='daytimeName' />
+          <XAxis
+            interval={0}
+            tick={width < 500 ? <RotatedAxisTick fontSize={fontSize}/> : {fontSize}}
+            dataKey='daytimeName' />
           <YAxis domain={[0, 10]} ticks={[0, 2, 4, 6, 8, 10]} />
           <Tooltip />
           {daytimeDatasets.map((_, index) => {
             const key = statDisplayName
-            return <Bar key={key} dataKey={key} barSize={5} fill={AppColors.barSeries[index % AppColors.barSeries.length]} />
+            return <Bar key={key} dataKey={key} fill={AppColors.barSeries[index % AppColors.barSeries.length]} />
           })}
         </BarChart>
       </div>
