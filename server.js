bf2a971053cb138a87d20a49f8e2720d65cf8c1f
Michael Manhard <mmanhard93@gmail.com>
2020-09-02 01:48:37 -0500

    -Require HTTPS requests outside of development.

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const app = express();
 const publicPath = path.join(__dirname, "public");
 const port = process.env.PORT || 3000;
 
+// If app not in development, redirect not 'https' requests to https.
+// If in development, allow non-'https' requests.
+function requireHTTPS(req, res, next) {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS);
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
